02f66f76e2da84d1f8f7ef73c58c056bce3191ca
Familjen Fornander <familjen@fornandmacbook5.home>
2026-01-18 23:10:45 +0100

    Add CLI-command list

--- app/commands/list.py
+++ app/commands/list.py
@@ -1,32 +1,15 @@
 import typer
 
-app = typer.Typer(help = "Lista alla eller några av predikningarna.")
 
-#(limit: int = typer.Option(10, '--limit', '-n'), all: str = typer.Option('all', '--all', '-a'), sort: str = typer.Option('code', '--sort', '-s'), reversed: bool = typer.Option(False, '--reversed', '-r')):
+app = typer.Typer(help = "Lista de senaste predikningarna", invoke_without_command=True)
+#   sermon list [--limit N] [--all] [--date]
 
-
-
-
-
-# Update list function. Syntax:
-
-#   sermon list [--limit N] [--sort FIELD] [--reversed True]
-
-#Need to rewrite the two functions below to a single and interpret the options.
-#Add option like [code | date | title] etc?
-
-
-
-
-@app.command()
-def all():
-    """Lista alla predikningar"""
-    print("Listar alla predikningar")
-
-@app.command()
-def recent(n: int = 5):
-    """Lista senaste n predikningar"""
-    print(f"Listar {n} senaste predikningarna")
-
-
+@app.callback()
+def list_sermons(
+        limit: int = typer.Option(10, '--limit', '-n', help='Antal predikningar att visa'),
+        all: bool = typer.Option(False, '--all', help='Visa alla predikningar'), 
+        date: bool = typer.Option(False, '--date', help='Sorterat efter datum för framförande istället för kod')
+        ):
+    """Lista alla eller några av predikningarna"""
+    print("Listar predikningar")
 
