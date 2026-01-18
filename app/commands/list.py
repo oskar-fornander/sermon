d90104051767b4d8488365de677473e137b3f32e
Familjen Fornander <familjen@fornandmacbook5.home>
2026-01-18 15:24:38 +0100

    Start changing list command

--- app/commands/list.py
+++ app/commands/list.py
@@ -2,6 +2,22 @@ import typer
 
 app = typer.Typer(help = "Lista alla eller några av predikningarna.")
 
+#(limit: int = typer.Option(10, '--limit', '-n'), all: str = typer.Option('all', '--all', '-a'), sort: str = typer.Option('code', '--sort', '-s'), reversed: bool = typer.Option(False, '--reversed', '-r')):
+
+
+
+
+
+# Update list function. Syntax:
+
+#   sermon list [--limit N] [--sort FIELD] [--reversed True]
+
+#Need to rewrite the two functions below to a single and interpret the options.
+#Add option like [code | date | title] etc?
+
+
+
+
 @app.command()
 def all():
     """Lista alla predikningar"""
