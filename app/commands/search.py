1d8c5c7628ffd0dd049204dc1568ddf14f4f5799
Oskar Fornander <oskar.fornander@gmail.com>
2026-03-23 22:10:59 +0100

    Start working on search command

--- app/commands/search.py
+++ app/commands/search.py
@@ -1,20 +1,46 @@
 
 import typer
+from typing import Optional, Literal
 from app.presentation.common import clear_screen
 from app.services.search_sermons import search_sermons
 
+
+# No search with regular expressions.
+# Implement in the future? 
+#   sermon search title:nåd
+#   Search multiple words with AND: sermon search "nåd tro" -> nåd AND tro
 
 
 
 def search(
         search_term: str, 
-        field: str = typer.Option('all', '--field', '-f'), 
-        regex: bool = typer.Option(False, '--regex', '-r')):
+        limit: int = typer.Option(10, '--limit', '-n', help='Antal predikningar att visa'),
+        all: bool = typer.Option(True, '--all', help='Visa alla predikningar'), 
+        offset: int = typer.Option(0, '--offset', help='Offset från senaste predikan'),
+        #sort: str = typer.Option("code", "--sort", help="Sortera efter predikokod 'code' eller datum 'date'"),
+        sort: Literal['code', 'date'] = typer.Option('code', '--sort', help="Sortera efter predikokod 'code' eller datum 'date'"),
+        reverse: bool = typer.Option(False, '--reverse', '-r', help='Omvänd sortering'),
+
+        year: Optional[int] = typer.Option(None, '--year', help='Filtrera efter år'),
+        month: Optional[str] = typer.Option(None, '--month', help='Filtrera efter månad'),
+        place: Optional[str] = typer.Option(None, '--place', help='Filtrera efter plats'),
+        report: Optional[str] = typer.Option(None, '--report', help='Filtrera efter omdöme (A, B, C)'),
+        has_recording: bool = typer.Option(None, '--has-recording', help='Visa endast predikningar med inspelning')
+        ):
+
     """Sök bland predikningarna"""
+
+    clear_screen()
     print(f"Sök på '{search_term}' bland predikningarna")
 
+    if all:
+        limit = 0  # Display all
+        offset = 0  # No offset in search
 
-    search_sermons(search_term)
+    if sort == 'date':
+        search_sermons(search_term=search_term, list_by='date', n=limit, offset=offset, reverse=reverse, year=year, month=month, place=place, report=report, must_have_recording=has_recording)  # List sermons by service dates
+    else:
+        search_sermons(search_term=search_term, list_by='code', n=limit, offset=offset, reverse=reverse, year=year, month=month, place=place, report=report, must_have_recording=has_recording)  # List sermons by sermon code
 
 
 
--- app/services/search_sermons.py
+++ app/services/search_sermons.py
@@ -3,9 +3,8 @@ from app.db import query_sermons
 from app.presentation.common import console
 
 
-def search_sermons(search_term):
+def search_sermons(search_term = '', list_by='code', n=0, offset=0, reverse = False, year = None, month = None, place = None, report = None, must_have_recording = False):  # List sermons by sermon code
     """Search and filter the sermons."""
-    pass
 
     # send parameters as lower case?
 
