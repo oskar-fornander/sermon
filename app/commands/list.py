044a4b7386068d6f72ac91b7eea9ac35db60ffcf
Oskar Fornander <oskar.fornander@gmail.com>
2026-04-05 16:49:02 +0200

    remove --offset

--- app/commands/list.py
+++ app/commands/list.py
@@ -15,7 +15,6 @@ def sermon_listing_function(
         all: bool = typer.Option(False, '--all', help='Visa alla predikningar'), 
         date_from: str = typer.Option('', '--from', help='Visa predikningar senare än detta datum'),
         date_to: str = typer.Option('', '--to', help='Visa predikningar före detta datum'),
-        offset: int = typer.Option(0, '--offset', help='Offset från senaste predikan'),
         sort: Literal['code', 'date'] = typer.Option('code', '--sort', help="Sortera efter predikokod 'code' eller datum 'date'"),
         reverse: bool = typer.Option(False, '--reverse', '-r', help='Omvänd sortering'),
 
@@ -33,11 +32,10 @@ def sermon_listing_function(
     
     if all:
         limit = 0  # Display all
-        offset = 0  # No offset in search
 
     if sort == 'date':
-        list_sermons(list_by='date', n=limit, offset=offset, reverse=reverse, date=date, date_from=date_from, date_to=date_to, year=year, month=month, place=place, report=report, must_have_recording=has_recording)  # List sermons by service dates
+        list_sermons(list_by='date', n=limit, reverse=reverse, date=date, date_from=date_from, date_to=date_to, year=year, month=month, place=place, report=report, must_have_recording=has_recording)  # List sermons by service dates
     else:
-        list_sermons(list_by='code', n=limit, offset=offset, reverse=reverse, date=date, date_from=date_from, date_to=date_to, year=year, month=month, place=place, report=report, must_have_recording=has_recording)  # List sermons by sermon code
+        list_sermons(list_by='code', n=limit, reverse=reverse, date=date, date_from=date_from, date_to=date_to, year=year, month=month, place=place, report=report, must_have_recording=has_recording)  # List sermons by sermon code
 
 
