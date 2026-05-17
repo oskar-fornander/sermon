8f44e657aba8e10962ed649875fd82ae9b054457
Oskar Fornander <oskar.fornander@gmail.com>
2026-05-17 21:42:58 +0200

    update html style

--- app/templates/sermon.js
+++ app/templates/sermon.js
@@ -2,14 +2,21 @@
 const table = document.querySelector('table#sermon');
 const searchInput = document.getElementById("searchInput");
 const searchResult = document.getElementById("searchResult");
-let lastFilter = '';
+let lastQuery = '';
 
 
 let heading, rows, rowsSermons, rowsDetails;
 setup();
 sortTable(0);  // Make sure to sort it from start
-searchInput.addEventListener("input", searchSermons);
 searchInput.focus();
+
+let timeout;
+searchInput.addEventListener("input", () => {  // This works as a debounce
+  clearTimeout(timeout);
+  timeout = setTimeout(() => {
+      searchSermons();
+  }, 500);
+});
 
 
 
@@ -35,6 +42,7 @@ function setup() {
         }
     });
 
+    showNumberOfHits();
 }
 
 function toggleData(index) {
@@ -136,21 +144,30 @@ function clearSearch() {
 
 function searchSermons() {
 
-    //Add feature: combined search: nåd + sanning
     
-    let filter = searchInput.value.toLowerCase();
+    // Sökning          Betyder
+    // -----------------------------------
+    // tro kärlek	    tro AND kärlek
+    // "tro och kärlek"	exakt fras
+    // tro -kärlek	    tro AND NOT kärlek
+    // -hopp	        exkludera hopp
+    //
+    //
+    
+    let query = searchInput.value.trim().toLowerCase();
 
-    if (filter == lastFilter) return;  // Save some work in vain
-    lastFilter = filter;
-
-    let regex = RegExp.escape(filter);  // Escape string 
-    regex = new RegExp(`(${regex})`, "gi");
+    if (query == lastQuery) return;  // Save some work in vain
+    lastQuery = query;
+    unclosedQuotes = false;
+    if ((query.match(/[\'\"]/g) || []).length % 2 == 1) {  // Unclosed quotes
+        query = query.replace(/[\'\"]/g, '');  //Ignore quotes when not fully closed
+        unclosedQuotes = true;
+    }
 
     showNumberOfHits();
-
     clearHighlight();  // remove all highlights
 
-    if (filter.trim() == '' || filter.length < 3) {  //If no search string + avoid searching single letters
+    if (query.trim() == '' || query.length < 3 || unclosedQuotes) {  //If no search string + avoid searching single letters
         searchInput.classList.add('inactive');
         rows.forEach(row => {
             row.classList.remove('search-hidden');  // show all
@@ -160,22 +177,68 @@ function searchSermons() {
         return;
     }
     searchInput.classList.remove('inactive');
+    //console.log('search');
 
     rows.forEach((x) => {  // Hide all rows with no hit
         x.classList.add('search-hidden');
         x.classList.remove('force-expanded');
     });
 
+
+    let phrases = query.match(/['"]([^'"]+)['"]/g) || [];  // Extract phrases in quotes
+    phrases = phrases.map(p => p.replace(/['"]/g, ""));
+    query = query.replace(/['"]([^'"]+)['"]/g, "");  // Remove phrases from query
+
+    let terms = query.split(/\s+/);  // Other terms in query
+    let must = [];
+    let mustNot = [];
+    terms.forEach(t => {
+      if (t.startsWith("-")) {
+        mustNot.push(t.slice(1));  // a word to exclude
+      } else if (t) {
+        must.push(t);  // a word to AND
+      }
+    });
+
+    // Make regular expression of each term
+    const makeRegExp = (x) => {
+        let regex = RegExp.escape(x);
+        regex = new RegExp(`(${regex})`, "gi");
+        return regex;
+    };
+    highlightSearch = new RegExp(`(${phrases.concat(must).sort((a, b) => b.length - a.length).map(RegExp.escape).join('|')})`, "gi");  //Build a regex for highlight search: all words to show, sorted by length, escaped and regex like (this|or|that).
+
+    phrases = phrases.map(makeRegExp);
+    must = must.map(makeRegExp);
+    mustNot = mustNot.map(makeRegExp);
+
+    const textMatch = (text) => {
+        // phrases
+        if (!phrases.every(p => text.match(p))) return false;
+        // AND
+        if (!must.every(t => text.match(t))) return false;
+        // NOT
+        if (mustNot.some(t => text.match(t))) return false;
+        return true;
+    };
+
+    const getSearchableText = (row) => {
+        const clone = row.cloneNode(true);
+        clone.querySelectorAll('.no-search').forEach(el => el.remove());  // Exclude non searchable nodes
+        return clone.textContent.toLowerCase();
+    }
+
     rows.forEach(row => {
-        let text = row.textContent.toLowerCase();
-        if (text.match(regex)) {  // a hit
+        //let text = row.textContent.toLowerCase();
+        let text = getSearchableText(row);
+        if (textMatch(text)) {
             const index = row.dataset.index;
             const rowSermon = table.querySelector("tr[data-index='" + index + "']");
             const rowDetails = table.querySelector("tr.details[data-index='" + index + "']");
             rowSermon.classList.remove('search-hidden');  // Show both main row and deatils row
             rowDetails.classList.remove('search-hidden');
             if (row.classList.contains('details') && !row.classList.contains('duplicate')) row.classList.add('force-expanded');  // Force the details row to be visible if the serach hit is here
-            highlight([rowSermon, rowDetails], regex);
+            highlight([rowSermon, rowDetails], highlightSearch);
         }
     });
 
@@ -183,7 +246,7 @@ function searchSermons() {
 }
 
 function showNumberOfHits() {
-    let hits = table.querySelectorAll("tr:not(.details, .hidden, .search-hidden, .duplicate, .heading)").length;
+    let hits = table.querySelectorAll("tr:not(.details, .hidden, .search-hidden, .duplicate, thead > tr)").length;
     searchResult.innerHTML = hits == 1? hits + ' träff': hits + ' träffar';
 }
 
@@ -201,34 +264,36 @@ function clearHighlight() {
 
 function highlight(rows, regex) {
     //...txt... ->  ...<span class="highlight">txt</span>...
+    //
 
-    rows.forEach(row => {
-        const walker = document.createTreeWalker(row, NodeFilter.SHOW_TEXT, null, false);  // Create a node tree walker to visit only text nodes
-        let currentNode;
-        const nodesToProcess = [];
-        while (currentNode = walker.nextNode()) {
-            if (currentNode.textContent.match(regex)) {
-                nodesToProcess.push(currentNode);
-            }
+    const excludeNode = (node) => {
+        let el = node.parentElement;
+        while (el) {
+            if (el.classList && el.classList.contains('no-search')) return true;
+            el = el.parentElement;
         }
-        nodesToProcess.forEach(node => {
-            const parent = node.parentNode;
-            const parts = node.textContent.split(regex);
-            const fragment = document.createDocumentFragment();
+        return false;
+    };
+    
+    rows.forEach(row => {
+        const walker = document.createTreeWalker(row, NodeFilter.SHOW_TEXT, null, false);
+        let node;
+        const nodes = [];
 
-            parts.forEach(part => {
-              if (part.match(regex)) {
-                const span = document.createElement('span');
-                span.className = 'highlight';  // Set highlighting on new element
-                span.textContent = part;
-                fragment.appendChild(span);
-              } else if (part !== "") {
-                fragment.appendChild(document.createTextNode(part));
-              }
-            });
-            parent.replaceChild(fragment, node);
+
+        while (node = walker.nextNode()) {  // Collect nodes
+            if (excludeNode(node)) continue;  // Exclude heading nodes
+            nodes.push(node);
+        }
+
+        nodes.forEach(node => {  // Manipulate DOM
+            const replaced = node.nodeValue.replace(regex, '<span class="highlight">$1</span>');
+            if (replaced !== node.nodeValue) {
+                const span = document.createElement("span");
+                span.innerHTML = replaced;
+                node.replaceWith(...span.childNodes);
+            }
         });
     });
 }
-
 
