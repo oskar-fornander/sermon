4ae0deb49f6bf71bfa1a50efbc0745f48e8d9688
Oskar Fornander <oskar.fornander@gmail.com>
2026-06-15 11:02:20 +0200

    Replace print() with console.print()

--- app/commands/export.py
+++ app/commands/export.py
@@ -1,5 +1,5 @@
 import typer
-from app.presentation.common import clear_screen
+from app.presentation.common import clear_screen, console
 from app.services.export_html import export_html
 from app.services.podcast import publish_episode
 
@@ -10,14 +10,14 @@ app = typer.Typer(help = 'Exportera registret som html eller enskild predikan so
 @app.command()
 def html():
     """Exportera predikoregistret som html."""
-    print("Exportera listan av predikningar som html")
+    console.print("Exportera listan av predikningar som html")
     export_html()
 
 
 @app.command()
 def podcast(sermon_code: str):
     """Exportera en predikan som podcast."""
-    print(f"Exportera predikan {sermon_code} som podcast.")
+    console.print(f"Exportera predikan {sermon_code} som podcast.")
     publish_episode(sermon_code)
 
     # sermon export podcast P382
